7969bc5e46ee5fc5ddd7e01cb95248130035f1be
Vinícius Cipriano <110792258+viniciuscipriano@users.noreply.github.com>
2024-02-21 21:14:45 +0000

    change main.js

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 function addTabela() {
-    let dados = document.getElementById("dados").value;
+    let botao = document.getElementById("nome").value;
 
-    let botao = document.getElementById("botao");
-
-    var arraydados = [];
-
-
-
-    // console.log("arraydados");
-
+    alert(botao);
 }
