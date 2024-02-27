57ff8ff80b94369d927e7b91698b3e8a8bcf9ede
Vinícius Cipriano <110792258+viniciuscipriano@users.noreply.github.com>
2024-02-27 00:24:12 +0000

    finish project

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 function addTabela() {
-    let botao = document.getElementById("nome").value;
+    const nome = document.getElementById("nome").value;
+    const nota = document.getElementById("nota").value;
 
-    alert(botao);
+    const arrayTabela = [];
+    arrayTabela.push(nome, nota);
+
+    let tbody = document.getElementById("tbody");
+    let thead = document.getElementById("thead");
+
+    thead.innerText = "";
+
+    let tr1 = thead.insertRow();
+    let th_nome = tr1.insertCell();
+    let th_nota = tr1.insertCell();
+
+    th_nome.innerText = "NOME";
+    th_nota.innerText = "NOTA";
+
+    for (var i = 0; i < arrayTabela.length; i += 2) {
+        let tr = tbody.insertRow();
+
+        let td_nome = tr.insertCell();
+        let td_nota = tr.insertCell();
+
+        td_nome.innerText = arrayTabela[i];
+        td_nota.innerText = arrayTabela[i + 1];
+
+
+    }
+
+    document.getElementById("nome").value = "";
+    document.getElementById("nota").value = "";
+
+    return false;
 }
